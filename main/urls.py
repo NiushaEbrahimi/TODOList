0bccd659ccbb5cb6409ef9033374a12d8d54197f
NiushaEbrahimi <n.ebrahiminoor@gmail.com>
2025-09-20 10:01:59 -0700

    fix the edit and create

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ToDoListView, ToDoDetailView, home_view
+from .views import ToDoListView, ToDoDetailView, ToDoDetailCreate, home_view
 
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('Lists/', ToDoListView.as_view(), name='Lists'),
-    path('Lists/<int:pk>/', ToDoDetailView.as_view(), name='ListDetail'),
-
+    path('Lists/<int:pk>/', ToDoDetailView.as_view(), name='todolist_detail'),
+    path('Lists/create/', ToDoDetailCreate.as_view(), name='todolist_create'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import ToDoList, ItemToDo
 from django.views import View
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -20,11 +20,32 @@ class ToDoDetailView(DetailView):
     context_object_name = 'lists'
     queryset = ToDoList.objects.all()
     # queryset2 = ItemToDo.objects.all()
-
+    success_url = '/'
     def get_context_data(self, **kwargs):
         context =super().get_context_data(**kwargs)
         context['items'] = ItemToDo.objects.filter(todolist=self.object)
         return context
     
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        item_id = request.POST.get("item_id")
+        if item_id:
+            item = get_object_or_404(ItemToDo, id=item_id, todolist=self.object)
+            item.is_completed = not item.is_completed
+            item.save()
+        return redirect("todolist_detail", pk=self.object.pk)
+
+class ToDoDetailCreate(CreateView):
+    model = ToDoList
+    template_name = 'main/todolist_detail.html'
+    context_object_name = 'lists'
+    queryset = ToDoList.objects.all()
+    fields = ['title']
+    success_url = '/'
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+    
+
 def home_view(request, *args, **kwargs):
     return render(request, 'main/home.html', {})
